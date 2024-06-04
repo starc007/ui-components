e5ca85d2060c432e9abf6fd9859da91645145ed2
Saurabh Chauhan <36479565+starc007@users.noreply.github.com>
2024-06-04 15:12:54 +0530

    fix: update package nd next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  distDir: "build",
+  cleanDistDir: true,
+  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  trailingSlash: true,
+};
 
 export default nextConfig;
