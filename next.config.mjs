fa6211cf8d90e2ca245550d5dd5b57900a1cf2af
Saurabh Chauhan <36479565+starc007@users.noreply.github.com>
2024-06-04 15:26:03 +0530

    fix: update eslint

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
-  cleanDistDir: true,
-  // Optional: Add a trailing slash to all paths `/about` -> `/about/`
-  trailingSlash: true,
+  //   distDir: "build",
+  //   cleanDistDir: true,
+  //   // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+  //   trailingSlash: true,
 };
 
 export default nextConfig;
